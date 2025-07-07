adf89a18710ae82153288d1e0b9541ee987a8015
AJmight <arnoldmugahi@gmail.com>
2025-07-07 23:09:06 +0300

    Initial commit

---
+++ users/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import get_user_model
+from django.views import View
+
+# Create your views here.
